564cd4dee6006c60192505f21a86673455088d84
zouhair-pro <79086236+zouhair-pro@users.noreply.github.com>
2021-03-31 15:52:36 +0100

    .

--- android/app/src/main/java/com/twiliovideocall/MainApplication.java
+++ android/app/src/main/java/com/twiliovideocall/MainApplication.java
@@ -23,11 +23,12 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-            ...
-            new TwilioPackage()
-        );
-    }
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          return packages;
+        }
 
         @Override
         protected String getJSMainModuleName() {
